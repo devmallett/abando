9651036e89201ac98c7348efa63ec6a10b5e0bad
devmallett <34248685+devmallett@users.noreply.github.com>
2021-10-30 13:57:13 -0400

    isolated ticker and price

--- fin_scraper.py
+++ fin_scraper.py
@@ -10,15 +10,22 @@ import pandas as pd
 # soup = BeautifulSoup(fin_request.content, 'html5lib')
 
 # print(soup.prettify())
-headers = {"User-Agent" : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"}
+fin_headers = {"User-Agent" : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"}
 # req = requests.get("https://finviz.com/quote.ashx?t=FB" ,headers = headers)
-req = requests.get("https://finviz.com/screener.ashx?v=111&f=sh_avgvol_o1000,sh_price_u20,ta_pattern_tlresistance&ft=4" ,headers = headers).text
+fin_viz_request = requests.get("https://finviz.com/screener.ashx?v=111&f=sh_avgvol_o1000,sh_price_u20,ta_pattern_tlresistance&ft=4" ,headers = fin_headers).text
 
-table = pd.read_html(req)
+pattern_one = pd.read_html(fin_viz_request)
+pattern_one = pattern_one[-2]
+pattern_one.columns = pattern_one.iloc[0]
+# pattern_one.columns = pattern_one.loc[[ "Ticker" ] ,["Price"]]
+pattern_one = pattern_one[1:]
+cols = [1 ,8]
+iso = pattern_one[pattern_one.columns[cols]]
+# rows = pattern_one.iloc[2:len(pattern_one)]
+# rows = pattern_one.columns[[" Ticker "] ,[" Price "]]
 
-
-
-print(table[-2])
+print(iso)
+# print(pattern_one)
 
 
 
