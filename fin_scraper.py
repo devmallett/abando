bb0b8abdd86956f709276aee53cfffa5dfdf9924
devmallett <34248685+devmallett@users.noreply.github.com>
2021-10-30 12:40:37 -0400

    Pulling vlaues down and formatting them into table using pandas

--- fin_scraper.py
+++ fin_scraper.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
 
 # main = 'https://finviz.com/screener.ashx?v=111&f=sh_avgvol_o1000,sh_price_u20,ta_pattern_tlresistance&ft=4' 
@@ -10,9 +11,19 @@ from bs4 import BeautifulSoup
 
 # print(soup.prettify())
 headers = {"User-Agent" : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"}
-req = requests.get("https://finviz.com/quote.ashx?t=FB" ,headers = headers)
-soup = BeautifulSoup(req.content, 'html.parser')
+# req = requests.get("https://finviz.com/quote.ashx?t=FB" ,headers = headers)
+req = requests.get("https://finviz.com/screener.ashx?v=111&f=sh_avgvol_o1000,sh_price_u20,ta_pattern_tlresistance&ft=4" ,headers = headers).text
 
+table = pd.read_html(req)
+
+
+
+print(table[-2])
+
+
+
+
+# soup = BeautifulSoup(req.content, 'html.parser')
 # table = soup.find_all(lambda tag: tag.name=='table')
 # rows = table[8].findAll(lambda tag: tag.name=='tr')
 # out=[]
@@ -22,4 +33,4 @@ soup = BeautifulSoup(req.content, 'html.parser')
 #     print(out)
 
 # print(table)
-print(soup.prettify())
+# print(soup.prettify())
