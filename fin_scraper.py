29380de346157fe0a74f3f7974508d7b61f9f307
devmallett <34248685+devmallett@users.noreply.github.com>
2021-10-30 17:50:57 -0400

    individual prices

--- fin_scraper.py
+++ fin_scraper.py
@@ -3,41 +3,32 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 
-# main = 'https://finviz.com/screener.ashx?v=111&f=sh_avgvol_o1000,sh_price_u20,ta_pattern_tlresistance&ft=4' 
-#     # links.append(main)
-# fin_request = requests.get(main)
-# # state_pull = BeautifulSoup(state_request.content ,'html5lib')
-# soup = BeautifulSoup(fin_request.content, 'html5lib')
+# def fin_one():
 
-# print(soup.prettify())
-fin_headers = {"User-Agent" : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"}
-# req = requests.get("https://finviz.com/quote.ashx?t=FB" ,headers = headers)
-fin_viz_request = requests.get("https://finviz.com/screener.ashx?v=111&f=sh_avgvol_o1000,sh_price_u20,ta_pattern_tlresistance&ft=4" ,headers = fin_headers).text
+fin_headers = {
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"}
+fin_viz_request = requests.get(
+    "https://finviz.com/screener.ashx?v=111&f=sh_avgvol_o1000,sh_price_u20,ta_pattern_tlresistance&ft=4", headers=fin_headers).text
 
 pattern_one = pd.read_html(fin_viz_request)
 pattern_one = pattern_one[-2]
 pattern_one.columns = pattern_one.iloc[0]
-# pattern_one.columns = pattern_one.loc[[ "Ticker" ] ,["Price"]]
 pattern_one = pattern_one[1:]
 cols = [1 ,8]
 iso = pattern_one[pattern_one.columns[cols]]
-# rows = pattern_one.iloc[2:len(pattern_one)]
-# rows = pattern_one.columns[[" Ticker "] ,[" Price "]]
-
-print(iso)
-# print(pattern_one)
 
 
+#TODO
+#return ticker and prices that are higher than 15 dollars
 
 
-# soup = BeautifulSoup(req.content, 'html.parser')
-# table = soup.find_all(lambda tag: tag.name=='table')
-# rows = table[8].findAll(lambda tag: tag.name=='tr')
-# out=[]
-# for i in range(len(rows)):
-#     td=rows[i].find_all('td')
-#     out=out+[x.text for x in td]
-#     print(out)
+for price in range(1 ,len(iso["Price"])):
+    print(iso["Price"][price])
 
-# print(table)
-# print(soup.prettify())
+
+# print(iso["Price"][1])
+    
+
+
+
+# print(iso)
