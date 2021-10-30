359e7ffa1be8ae79ccf4fce8ac33d3efedfdcb3b
devmallett <34248685+devmallett@users.noreply.github.com>
2021-10-30 18:07:41 -0400

    patterns added

--- fin_scraper.py
+++ fin_scraper.py
@@ -3,26 +3,73 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 
-# def fin_one():
 
-fin_headers = {
-    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"}
-fin_viz_request = requests.get(
-    "https://finviz.com/screener.ashx?v=111&f=sh_avgvol_o1000,sh_price_u20,ta_pattern_tlresistance&ft=4", headers=fin_headers).text
+def pattern_one():
 
-pattern_one = pd.read_html(fin_viz_request)
-pattern_one = pattern_one[-2]
-pattern_one.columns = pattern_one.iloc[0]
-pattern_one = pattern_one[1:]
-cols = [1 ,8]
-iso = pattern_one[pattern_one.columns[cols]]
+    fin_headers = { #global in class
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"}
+    fin_viz_request = requests.get(
+        "https://finviz.com/screener.ashx?v=111&f=sh_avgvol_o1000,sh_price_u20,ta_pattern_tlresistance&ft=4", headers=fin_headers).text
+
+    pattern_one = pd.read_html(fin_viz_request)
+    pattern_one = pattern_one[-2]
+    pattern_one.columns = pattern_one.iloc[0]
+    pattern_one = pattern_one[1:]
+    cols = [1 ,8]
+    iso = pattern_one[pattern_one.columns[cols]]
+
+    open_list = []
+
+    for price in range(1 ,len(iso["Price"])): #global in class
+        if int(float(iso["Price"][price])) > 15:
+            open_list.append(iso["Price"][price])
+            print(open_list)
+
+def pattern_two():
+
+    fin_headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"}
+    fin_viz_request = requests.get(
+        "https://finviz.com/screener.ashx?v=111&f=earningsdate_nextweek,sh_price_u20,ta_pattern_channelup&ft=4", headers=fin_headers).text
+
+    pattern_two = pd.read_html(fin_viz_request)
+    pattern_two = pattern_two[-2]
+    pattern_two.columns = pattern_two.iloc[0]
+    pattern_two = pattern_two[1:]
+    cols = [1 ,8] #global in class
+    iso = pattern_two[pattern_two.columns[cols]]
+
+    open_list = [] #global in calss
+
+    for price in range(1 ,len(iso["Price"])):
+        if int(float(iso["Price"][price])) > 15:
+            open_list.append(iso["Price"][price])
+            print(open_list)
 
 
-for price in range(1 ,len(iso["Price"])):
-    if int(float(iso["Price"][price])) > 15:
-        print(iso["Price"][price])
+def pattern_three():
+
+    fin_headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"}
+    fin_viz_request = requests.get(
+        "https://finviz.com/screener.ashx?v=111&f=earningsdate_nextweek,fa_pe_profitable,sh_avgvol_o1000,sh_price_u30,ta_pattern_tlresistance&ft=4", headers=fin_headers).text
+
+    pattern_three = pd.read_html(fin_viz_request)
+    pattern_three = pattern_three[-2]
+    pattern_three.columns = pattern_three.iloc[0]
+    pattern_three = pattern_three[1:]
+    cols = [1 ,8]
+    iso = pattern_three[pattern_three.columns[cols]]
+
+    open_list = []
+
+    for price in range(1 ,len(iso["Price"])):
+        if int(float(iso["Price"][price])) > 15:
+            open_list.append(iso["Price"][price])
+            print(open_list)
 
 
+# pattern_three()
 # print(iso["Price"][1])
     
 
