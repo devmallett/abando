c9a8374708f683e2664e820fe94be4ed4ad09ab9
devmallett <34248685+devmallett@users.noreply.github.com>
2021-11-02 01:03:52 -0400

    todos have been completed

--- fin_scraper.py
+++ fin_scraper.py
@@ -4,19 +4,14 @@ import pandas as pd
 import numpy as np
 
 
-
-
-#TODO
-# Abstract logic so that you only have to make function calls within the different patterns 
-# Map the keys and vlaues of those tickers and retrun them in a global object 
-
-
 class FinViz:
-    def __init__(self):
+    def __init__(self ):
         self.target_cols = [1 ,8]
         self.ticker_list = []
         self.price_list = []
-        self.pattern_one_list = {}
+        self.pattern_one_dict = {}
+        self.pattern_two_dict = {}
+        self.pattern_three_dict = {}
         self.headers = { 
         "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"}
 
@@ -47,28 +42,11 @@ class FinViz:
         
         
         for tickers ,prices in zip ( self.pull_tickers(iso) ,self.pull_prices(iso) ):
-            self.pattern_one_list[tickers] = prices
-        print(self.pattern_one_list)
+            self.pattern_one_dict[tickers] = prices
+        # print(self.pattern_one_dict)
+        return self.pattern_one_dict
             
-        
-        
-        
-        
-        
-        
-
-        # open_list = []
-        # print(iso["Ticker"])
-        
-        # for ticker in range(1 ,len(iso["Ticker"])):
-        #     self.open_list.append(iso["Ticker"][ticker])
-        #     print(self.open_list)
             
-        # for price in range(1 ,len(iso["Price"])): #global in class
-        #     if int(float(iso["Price"][price])) > 15:
-        #         self.open_list.append(iso["Ticker"][price])
-        #         print(self.open_list)
-
     def pattern_two(self):
 
         fin_viz_request = requests.get(
@@ -80,12 +58,10 @@ class FinViz:
         pattern_two = pattern_two[1:]
         iso = pattern_two[pattern_two.columns[self.target_cols]]
 
-        open_list = [] #global in calss
-
-        for price in range(1 ,len(iso["Price"])):
-            if int(float(iso["Price"][price])) > 15:
-                open_list.append(iso["Price"][price])
-                print(open_list)
+        for tickers ,prices in zip ( self.pull_tickers(iso) ,self.pull_prices(iso) ):
+            self.pattern_two_dict[tickers] = prices
+        # print(self.pattern_two_dict)
+        return self.pattern_two_dict
 
 
     def pattern_three(self):
@@ -99,12 +75,10 @@ class FinViz:
         pattern_three = pattern_three[1:]
         iso = pattern_three[pattern_three.columns[self.target_cols]]
 
-        open_list = []
-
-        for price in range(1 ,len(iso["Price"])):
-            if int(float(iso["Price"][price])) > 15:
-                open_list.append(iso["Price"][price])
-                print(open_list)
+        for tickers ,prices in zip ( self.pull_tickers(iso) ,self.pull_prices(iso) ):
+            self.pattern_three_dict[tickers] = prices
+        # print(self.pattern_three_dict)
+        return self.pattern_three_dict
 
 new_request = FinViz()
 
