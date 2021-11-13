390d7d8461ba24d802b500f6884a7393f47f533b
devmallett <34248685+devmallett@users.noreply.github.com>
2021-11-13 05:15:45 -0500

    wrapped tweets in a class

--- async_tester.py
+++ async_tester.py
@@ -1,25 +1,32 @@
-import asyncio
-from fin_scraper import FinViz
-
+# import asyncio
+# from fin_scraper import FinViz
+from tweet_screener import Tweets
 # new_ = FinViz()
 # odl = FinViz()
 
+
+yodl = Tweets()
+yodl.main()
 # new_.pattern_one()
 # odl.pattern_three()
 
 
 
-async def naming():
-    await foo("text")
-    print("Devin")
+
+
+# async def naming():
+#     # await foo("text")
+#     await new_.pattern_one()
+#     # print("Devin")
     
     
     
-async def foo(text):
-    # print(text)
-    await asyncio.sleep(1)
-    print(text)
+# async def foo():
+#     # print(text)
+#     # await asyncio.sleep(1)
+#     await odl.pattern_three()
+#     # print(text)
     
     
-asyncio.run(naming())
-# await naming()
+# asyncio.run(naming())
+# # await naming()
--- tweet_screener.py
+++ tweet_screener.py
@@ -7,14 +7,14 @@ import datetime
 from secrets import TWITTER_BEARER_TOKEN
 # from secrets import TWITTER_ACCESS_TOKEN
 # from secrets import TWITTER_ACCESS_TOKEN_SECRET
-from fin_scraper import FinViz
+# from fin_scraper import FinViz
 
 
-new_request = FinViz()
+# new_request = FinViz()
 
-this_ticker = new_request.pattern_one()
+# this_ticker = new_request.pattern_one()
 
-bearer_token = TWITTER_BEARER_TOKEN
+# bearer_token = TWITTER_BEARER_TOKEN
 
 #TODO
 #call a FinVIz pattern and return the tickers, then use the tickers in an api call to pull tickers associated with the tickers 
@@ -23,74 +23,84 @@ bearer_token = TWITTER_BEARER_TOKEN
 
 """
 
+class Tweets:
+    def __init__(self):
+        # self.ticker = None
+        self.bearer_token = TWITTER_BEARER_TOKEN
+        self.tweet_list = []
+
+    def create_url(self):
+        # for ticker ,price in this_ticker.items():
+        #     print(ticker ,"this is th key from items pulled")
+            
+            # return "https://api.twitter.com/2/tweets/search/recent?query={}".format(ticker)
+            print("URL was created")
+            return "https://api.twitter.com/2/tweets/search/recent?query=ROON"
+            
 
 
 
-def create_url():
-    # for ticker ,price in this_ticker.items():
-    #     print(ticker ,"this is th key from items pulled")
-        
-        # return "https://api.twitter.com/2/tweets/search/recent?query={}".format(ticker)
-        return "https://api.twitter.com/2/tweets/search/recent?query=ROON"
+    def bearer_oauth(self ,r):
+        """
+        Method required by bearer token authentication.
+        """
+
+        r.headers["Authorization"] = "Bearer {}".format(self.bearer_token)
+        # r.headers["User-Agent"] = "v2SampledStreamPython"
+        return r
 
 
-
-def bearer_oauth(r):
-    """
-    Method required by bearer token authentication.
-    """
-
-    r.headers["Authorization"] = "Bearer {}".format(bearer_token)
-    # r.headers["User-Agent"] = "v2SampledStreamPython"
-    return r
-
-
-def connect_to_endpoint(url):
-    tweet_list = []
-    response = requests.request("GET", url, auth=bearer_oauth)
-    print(response.status_code)
-    for response_line in response.iter_lines(): 
-            if response_line:
-                json_response = json.loads(response_line)
-                tweet_list = json_response['data']
-                #TODO 
-                # loop through an array of objects to pull text from each object
-                for each_item in tweet_list:
-                    for tweet_key ,tweet_val in each_item.items():
-                        if tweet_key == 'text':
+    def connect_to_endpoint(self ,url):
+        response = requests.request("GET", url, auth=self.bearer_oauth)
+        print(response.status_code)
+        for response_line in response.iter_lines(): 
+                if response_line:
+                    json_response = json.loads(response_line)
+                    self.tweet_list = json_response['data']
+                    #TODO 
+                    # loop through an array of objects to pull text from each object
+                    for each_item in self.tweet_list:
+                        for tweet_key ,tweet_val in each_item.items():
+                            if tweet_key == 'text':
+                                # print("{} : {}".format(key, val))
+                                print(tweet_val)
+                                # tweet_list.append(val)
+                                # print(tweet_list)
+                                
                             # print("{} : {}".format(key, val))
-                            print(tweet_val)
-                            # tweet_list.append(val)
-                            # print(tweet_list)
-                            
-                        # print("{} : {}".format(key, val))
-                    # print(tweet_list[each_item]['text'])
-                
-                
-                
-                
-                # for tweet in json_response['data']:
-                #     print(json_response['data'])
-                # print(json.dumps(json_response, indent=4, sort_keys=True))
-    if response.status_code != 200:
-        raise Exception(
-            "Request returned an error: {} {}".format(
-                response.status_code, response.text
+                        # print(tweet_list[each_item]['text'])
+                    
+                    
+                    
+                    
+                    # for tweet in json_response['data']:
+                    #     print(json_response['data'])
+                    # print(json.dumps(json_response, indent=4, sort_keys=True))
+        if response.status_code != 200:
+            raise Exception(
+                "Request returned an error: {} {}".format(
+                    response.status_code, response.text
+                )
             )
-        )
 
 
-def main():
-    url = create_url()
-    # timeout = 0
-    now = datetime.datetime.now()
-    timeout = now + datetime.timedelta(10)
-    # while True:
-    if now < timeout:
-        connect_to_endpoint(url)
-        # timeout += 1
+    def main(self):
+        url = self.create_url()
+        # timeout = 0
+        now = datetime.datetime.now()
+        timeout = now + datetime.timedelta(10)
+        # while True:
+        if now < timeout:
+            self.connect_to_endpoint(url)
+            # timeout += 1
 
 
-if __name__ == "__main__":
-    main()
+    if __name__ == "__main__":
+        print("This works")
+        main()
+        
+        
+        
+  
+    
 
