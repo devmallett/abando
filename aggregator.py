b42aa6e8771f3276f239b46116505acd877f6d62
devmallett <34248685+devmallett@users.noreply.github.com>
2021-11-14 22:51:36 -0500

    wrapped in method call

--- aggregator.py
+++ aggregator.py
@@ -6,13 +6,15 @@ from tweet_screener import Tweets
 
 
 
-# def runner():
-new_ = FinViz()
-tickers = {}
-tickers = new_.pattern_one()
-print(tickers , "this is the ticker dict")
-for ticker ,price in tickers.items():
-    print(ticker ,"this is th key from items pulled")
-# # yodl = Tweets("$AAPL")
-    yodl = Tweets(ticker)
-    yodl.main()
+def runner():
+    new_ = FinViz()
+    tickers = {}
+    tickers = new_.pattern_one()
+    print(tickers , "this is the ticker dict")
+    for ticker ,price in tickers.items():
+        print(ticker ,"this is th key from items pulled")
+    # # yodl = Tweets("$AAPL")
+        yodl = Tweets(ticker)
+        yodl.main()
+        
+runner()
