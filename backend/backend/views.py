5b065945010e4d64cb17b38dc149ec378adccce6
devmallett <34248685+devmallett@users.noreply.github.com>
2021-12-03 15:36:31 -0500

    frontend things

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+import requests 
 
 
 def index(request):
     return render(request ,'index.html')
+
+
+
+
+def home(request):
+    response=requests.get('https://api.covid19api.com/countries')
+    return render(request)
+
+# https://api.covid19api.com/countries
